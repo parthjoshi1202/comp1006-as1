d76348438dc7614c95a6c04a4bff2167fe4bd60d
Parth Joshi <pjoshi4@lakeheadu.ca>
2020-02-13 17:20:16 -0500

    Executing the Query and Using fetchAll() to store data

--- client-list.php
+++ client-list.php
@@ -7,8 +7,15 @@
 <body>
 <?php
 $database=new PDO('mysql:host=172.31.22.43;dbname=Parth1126914', 'Parth1126914', 'HE9auH3i9m');
-
 $query="SELECT * FROM gym";
+    
+$cmd=$database->prepare($query);
+$cmd->execute();
+
+
+
+$store_data=$cmd->fetchAll();
+echo '<table border="1"><thead><th>firstName</th><th>lastName</th><th>time_pref</th><th>membership</th></thead>';
 ?>
 </body>
 </html>
