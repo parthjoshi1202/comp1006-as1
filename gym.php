6f876dc17fdb22014f22935e1b7d073e83d087b8
Parth Joshi <pjoshi4@lakeheadu.ca>
2020-02-13 16:46:50 -0500

    Add files via upload

---
+++ gym.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register for Membership</title>
+</head>
+<body>
+    <form action="gym-saved.php" method="post">
+        <fieldset>
+            <label for="firstName">First Name: *</label>
+            <input name="firstName" id="firstName" required />
+        </fieldset>
+        <fieldset>
+            <label for="lastName">Last Name: *</label>
+            <input name="lastName" id="lastName" required />
+        </fieldset>
+        <fieldset>
+            <label for="time_pref">Preferred Time: *</label>
+            <input name="time_pref" id="time_pref" required />
+        </fieldset>
+
+        
+        <input type="submit" name="Add">
+    </form>
+
+</body>
+</html>
