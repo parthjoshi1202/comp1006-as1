c9d146a02b427bf1e34dc58ee76b6c982c05950d
Parth Joshi <pjoshi4@lakeheadu.ca>
2020-02-13 17:02:18 -0500

    Declaring Variables

--- gym-saved.php
+++ gym-saved.php
@@ -9,7 +9,14 @@
 //COMP 1006 Assignment-1
 //Name-Parth Joshi
 //Lakehead ID- 1126914
-
+$firstName=htmlspecialchars($_POST['firstName']);
+$lastName=htmlspecialchars($_POST['lastName']);
+$time_pref=htmlspecialchars($_POST['time_pref']);
+$membership=htmlspecialchars($_POST['membership']);
+    
+    
+    
+    
 ?>
 
 <form action="client-list.php" method="post">
