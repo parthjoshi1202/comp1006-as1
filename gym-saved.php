48ec947b90a7d74e9891b08a83283693d5f021b7
Parth Joshi <pjoshi4@lakeheadu.ca>
2020-02-13 17:00:18 -0500

    Add files via upload

---
+++ gym-saved.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Details Saved</title>
+</head>
+<body>
+<?php
+//COMP 1006 Assignment-1
+//Name-Parth Joshi
+//Lakehead ID- 1126914
+
+?>
+
+<form action="client-list.php" method="post">
+    <button>Click here for Client List</button>
+</form>
+
+</body>
+</html>
