d464ea813175589fbccde2eeb9a6b64009420a1f
Parth Joshi <pjoshi4@lakeheadu.ca>
2020-03-18 15:43:17 -0400

    Creating Index Page

---
+++ gym_index.php
@@ -0,0 +1,18 @@
+<?php
+$title='Welcome Page Gym';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Homepage</title>
+</head>
+<body>
+<h1>Welcome to 24/7 PHP Gym !</h1>
+<p>This site is written in PHP.SQL and JS </p>
+<p>This is a Platform for 24/7 Gym, Making People Happy, Healthy and Fit since 2020 ! </p>
+<p>Source Code: <a href="https://github.com/parthjoshi1202/comp1006-as1">github.com/parthjoshi1202/comp1006-as1</a> </p>
+
+</body>
+<footer>For COMP 1006 Assignment-2</footer>
+</html>
