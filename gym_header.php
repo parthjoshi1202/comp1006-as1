60bb8dea668d74de3dba0bc8512df6490fd940ed
Parth Joshi <pjoshi4@lakeheadu.ca>
2020-03-18 22:00:44 -0400

    Common Header for all Pages

---
+++ gym_header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Gym - <?php echo $title; ?></title>
+</head>
+<body>
+<nav>
+   <a href="gym_index.php">PHP 24/7 Gym</a>
+    <div>
+        <ul class="nav">
+            <li>
+                <a href="gym_index.php">Home</a>
+                <a href="client-list.php">Client List</a>
+
+            </li>
+        </ul>
+
+    </div>
+
+</nav>
+
+
+
+
