d918e49bc9d1cc685990d702d2fd9d5fb8c1fd54
Parth Joshi <pjoshi4@lakeheadu.ca>
2020-03-19 16:51:44 -0400

    Initializing Variables

--- gym.php
+++ gym.php
@@ -22,19 +22,44 @@
             <label for="membership">Select Membership Type: *</label>
             <select name="membership" id="membership" required />
             <?php
-        //COMP 1006 Assignment-1
-        //Name-Parth Joshi
-        //Lakehead ID- 1126914
-        $database= new PDO('mysql:host=172.31.22.43;dbname=Parth1126914', 'Parth1126914', 'HE9auH3i9m');            
-        $sql="SELECT membership FROM membership";
-        $cmd=$database->prepare($sql);
-        $cmd->execute();
-        $gym=$cmd->fetchAll();
-        foreach ($gym as $value) {
-        echo '<option>' . $value['membership'] . '</option>';   
-        }
-        $database=null;
-        ?>
+            //COMP 1006 Assignment-2
+            //Name-Parth Joshi
+            //Lakehead ID- 1126914
+            //initializing variables
+
+            $firstName=null;
+            $lastName=null;
+            $membership=null;
+            $memberId=null;
+
+            if(!empty($GET['memberId'])) {
+                $memberId = $_GET['memberId'];
+
+
+                $database = new PDO('mysql:host=172.31.22.43;dbname=Parth1126914', 'Parth1126914', 'HE9auH3i9m');
+                $sql = "SELECT membership FROM membership";
+                $cmd = $database->prepare($sql);
+                $cmd->execute();
+                $gym = $cmd->fetchAll();
+                foreach ($gym as $value) {
+                    echo '<option>' . $value['membership'] . '</option>';
+                }
+
+                $database = null;
+            }
+            else {
+                $database = new PDO('mysql:host=172.31.22.43;dbname=Parth1126914', 'Parth1126914', 'HE9auH3i9m');
+                $sql = "SELECT membership FROM membership";
+                $cmd = $database->prepare($sql);
+                $cmd->execute();
+                $gym = $cmd->fetchAll();
+                foreach ($gym as $value) {
+                    echo '<option>' . $value['membership'] . '</option>';
+                }
+
+                $database = null;
+            }
+            ?>
         </fieldset>
         <input type="submit" name="Add">
     </form>
