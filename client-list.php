17fb1c1b99edb34e9691f0cdfae1b5be0e830e04
Parth Joshi <pjoshi4@lakeheadu.ca>
2020-03-19 16:37:25 -0400

    Adding try, catch for Authentication

--- client-list.php
+++ client-list.php
@@ -13,8 +13,10 @@ if(!empty($_SESSION['memberId'])) {
     echo '<a href="gym.php">Add Client</a>';
 }
 
+try {
 //Connect
 $database=new PDO('mysql:host=172.31.22.43;dbname=Parth1126914', 'Parth1126914', 'HE9auH3i9m');
+$database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $query="SELECT * FROM gym";
 
 //using command variable to run the query and then executing it
@@ -25,20 +27,29 @@ $cmd->execute();
 $store_data=$cmd->fetchAll();
 
 //Displaying the Table with Add, Delete and Edit Links
-echo '<table border="1"><thead><th>First Name</th><th>Last Name</th><th>Preferred Time</th><th>Membership</th><th>Delete</th><th>Edit</th></thead>';
-echo '<a href="gym.php">Add Client</a>';
+    echo '<table border="1"><thead><th>First Name</th><th>Last Name</th><th>Preferred Time</th><th>Membership</th><th>Delete</th><th>Edit</th></thead>';
+    echo '<a href="gym.php">Add Client</a>';
 
+    foreach ($store_data as $value) {
+        echo '<tr><td>' . $value['firstName'] . '</td><td>' . $value['lastName'] . '</td><td>' . $value['time_pref'] . '</td><td>' . $value['membership'] . '</td><td><a href="delete-client.php?memberId=' . $value['memberId'] . '"class="btn btn-danger"
+    onclick="return confirmDelete();">Delete</a><td><a href="gym.php?memberId=",' . $value['memberId'] . '"class="btn btn-danger">Edit</a></td></tr>';
+    }
+       //Showing Edit and Delete Link for Authenticated Users
+      /*  if (!empty($_SESSION['userId'])) {
+            echo '<td><a href="delete-client.php?memberId=' . $value['memberId'] . '" class="btn btn-danger"
+                onclick="return confirmDelete();">Delete</a><td><a href="gym.php?memberId=",' . $value['memberId'] . '"class="btn btn-danger">Edit</a></td></td>';
+        }
 
-    foreach ($store_data as $value)
-{
-     echo '<tr><td>' .$value['firstName']. '</td><td>'  .$value['lastName']. '</td><td>'  .$value['time_pref']. '</td><td>' .$value['membership']. '</td><td><a href="delete-client.php?memberId='. $value['memberId'].'"class="btn btn-danger" 
-    onclick="return confirmDelete();">Delete</a></td></tr>';
-}
+        echo '</tr>';
+    }*/
 
-echo '</table>';
+    echo '</table>';
 
 //Disconnect
-$database=null;
-
+    $database = null;
+}
+catch (Exception $e) {
+    exit();
+}
 require_once('gym_footer.php');
 ?>
