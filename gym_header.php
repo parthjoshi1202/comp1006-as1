9a3030a2fa00a607bbad0a1940820fca6c500c02
Parth Joshi <pjoshi4@lakeheadu.ca>
2020-03-25 13:22:55 -0400

    Keeping Logout Hidden for Users who have not Logged in

--- gym_header.php
+++ gym_header.php
@@ -13,7 +13,17 @@
             <li>
                 <a href="gym_index.php">Home</a>
                 <a href="client-list.php">Client List</a>
-
+                <?php
+                session_start();
+                if(!empty($_SESSION['memberId'])) {
+                    echo '<li><a href="#">' . $_SESSION['username'] . '</a></li>
+                    <li><a href="gym_logout.php">Logout</a></li>';
+                }
+                else {
+                    echo '<li><a href="gym_register.php">Register</a></li>
+                    <li><a href="gym_login.php">Login</a></li>';
+                }
+                ?>
             </li>
         </ul>
 
