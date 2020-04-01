7cdfecab93d9c8abfc45370ae66a99f23aadfbaa
Parth Joshi <pjoshi4@lakeheadu.ca>
2020-04-01 17:22:28 -0400

    .htaccess File and 404.php File for any Errors

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Page Not Found';
+require_once 'gym_header.php';
+?>
+
+    <main>
+        <h1>Ooops</h1>
+
+        <p>Sorry! We couldn't find that page.</p>
+        <p>Please use one of the links on the Right.</p>
+    </main>
+
+<?php
+require_once 'gym_footer.php';
+?>
