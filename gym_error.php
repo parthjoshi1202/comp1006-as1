a8ca7919e1b9fd15b8b0080c75d051403cb74920
Parth Joshi <pjoshi4@lakeheadu.ca>
2020-04-02 13:10:13 -0400

    General Error File

---
+++ gym_error.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Error Page</title>
+    <link rel="stylesheet" href="css/css.css" />
+
+</head>
+<body>
+<main>
+    <h4>Something's not Right 🤔</h4>
+        <p>Please Refresh and Retry !</p>
+</main>
+
+<nav>
+    <a href="gym_index.php">PHP 24/7 Gym</a>
+    <div>
+        <ul class="nav">
+            <li>
+                <a href="gym_index.php">Home</a>
+                <a href="client-list.php">Client List</a>
+            </li>
+        </ul>
+
+    </div>
+
+</nav>
+
+</main>
+
+<script src="js/scripts.js" type="text/javascript"></script>
+
+</body>
+</html>
