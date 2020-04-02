268a3b26c2c1ef8bbc7d0bb1cb1772d8b2e41e20
Parth Joshi <pjoshi4@lakeheadu.ca>
2020-04-02 13:17:52 -0400

    Adding General Error Page with Addition of Image

--- client-list.php
+++ client-list.php
@@ -26,14 +26,15 @@ $cmd->execute();
 //using fetchAll() method to store the data
 $store_data=$cmd->fetchAll();
 
-//Displaying the Table with Add, Delete and Edit Links
-    echo '<table border="1"><thead><th>First Name</th><th>Last Name</th><th>Preferred Time</th><th>Membership</th><th>Delete</th><th>Edit</th></thead>';
-    //echo '<a href="gym.php">Add Client</a>';
+//Displaying the Table with Add, Delete and Edit Links with Photo
+    echo '<table border="1"><thead><th>First Name</th><th>Last Name</th><th>Preferred Time</th><th>Membership</th><th>Delete</th><th>Edit</th><th>Picture</th></thead>';
+    //echo '<table border="1"><thead><th>First Name</th><th>Last Name</th><th>Preferred Time</th><th>Membership</th><th>Delete</th><th>Edit</th></thead>';
+   // echo '<a href="gym.php">Add Client</a>';
 
- foreach ($store_data as $value) {
+    foreach ($store_data as $value) {
         echo '<tr>';
 
-       if (!empty($_SESSION['userId'])) {
+        if (!empty($_SESSION['userId'])) {
            // echo '<td>' . $value['firstName'] . '</td><td>' . $value['lastName'] . '</td><td>' . $value['time_pref'] . '</td><td>' . $value['membership'] . '</td>';
             echo '<td>' . $value['firstName'] . '</td><td>' . $value['lastName'] . '</td>' ;
         }
@@ -49,17 +50,28 @@ $store_data=$cmd->fetchAll();
        // echo '<td>' . $value['firstName'] . '</td><td>' . $value['lastName'] . '</td><td>' . $value['time_pref'] . '</td><td>' . $value['membership'] . '</td>';
 
 
+
         //Showing Edit and Delete Link for Authenticated Users
         if (!empty($_SESSION['userId'])) {
             echo '<td><a href="delete-client.php?memberId=' . $value['memberId'] . '" class="btn btn-danger"
-                onclick="return confirmDelete();">Delete</a><td><a href="gym.php?memberId=",' . $value['memberId'] . '"class="btn btn-danger">Edit</a></td></td>';
+                onclick="return confirmDelete();">Delete</a><td><a href="gym.php?memberId="' . $value['memberId'] . '"class="btn btn-danger">Edit</a></td></td>';
+        }
+
+        //echo '</tr>';
+
+        if (!empty($value['pic'])) {
+            echo '<td><img src="gym_upload/' . $value['pic'] . '" alt="User Image" class="thumbnail" />';
+        }
+        else {
+            echo '<td></td>';
         }
 
         echo '</tr>';
-
         /*echo '<tr><td>' . $value['firstName'] . '</td><td>' . $value['lastName'] . '</td><td>' . $value['time_pref'] . '</td><td>' . $value['membership'] . '</td><td><a href="delete-client.php?memberId=' . $value['memberId'] . '"class="btn btn-danger"
     onclick="return confirmDelete();">Delete</a><td><a href="gym.php?memberId=",' . $value['memberId'] . '"class="btn btn-danger">Edit</a></td></tr>';*/
     }
+
+
 
     echo '</table>';
 
@@ -67,7 +79,7 @@ $store_data=$cmd->fetchAll();
     $database = null;
 }
 catch (Exception $e) {
-    echo 'Please Refresh and try Again';
+    header('location:gym_error.php');
     exit();
 }
 require_once('gym_footer.php');
