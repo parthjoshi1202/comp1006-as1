984c68e1c2bd5860fda28ec9ecd874f1ff76a3ba
Parth Joshi <pjoshi4@lakeheadu.ca>
2020-04-02 13:27:03 -0400

    Adding pic Variable

--- gym-saved.php
+++ gym-saved.php
@@ -18,6 +18,9 @@ $lastName=htmlspecialchars($_POST['lastName']);
 $time_pref=htmlspecialchars($_POST['time_pref']);
 $membership=htmlspecialchars($_POST['membership']);
 $memberId = $_POST['memberId'];
+//adding image upload
+$pic = $_FILES['pic'];
+$picName = null;
     
 echo $firstName;    
 
@@ -33,6 +36,25 @@ if(empty($lastName)) {
     $ok=false;
 }
 
+if (!empty($pic['tmp_name'])) {
+    $picName = $pic['name'];
+    $tmp_name = $pic['tmp_name'];
+
+    //this checks for the actual type of file
+    $type = mime_content_type($tmp_name);
+
+    if ($type != 'image/jpeg' && $type != 'image/png') {
+        echo 'Image must be a .jpg or .png file !';
+        $ok = false;
+        exit();
+    }
+
+    $picName = session_id() . '-' . $picName;
+    move_uploaded_file($tmp_name, "gym_upload/$picName");
+    //move_uploaded_file($tmp_name, "img/artists/$photoName");
+}    
+    
+    
 if($ok==true) {
     try {
     $database = new PDO('mysql:host=172.31.22.43;dbname=Parth1126914', 'Parth1126914', 'HE9auH3i9m');
@@ -40,36 +62,37 @@ if($ok==true) {
     //set up SQL Insert command
 
          if(empty($memberId)) {
-        $sql = "INSERT INTO gym (firstName, lastName, time_pref, membership) VALUES (:firstName,:lastName,:time_pref,:membership)";
-    } else{
-        $sql="UPDATE gym SET firstName= :firstName, lastName= :lastName, time_pref=:time_pref, membership=:membership WHERE memberId=:memberId ";
-    }
-    
- 
+            $sql = "INSERT INTO gym (firstName, lastName, time_pref, membership, pic) VALUES (:firstName,:lastName,:time_pref,:membership,:pic)";
+        } else {
+            $sql = "UPDATE gym SET firstName= :firstName, lastName= :lastName, time_pref=:time_pref, membership=:membership, pic=:pic WHERE memberId=:memberId ";
+        }
 
 //creating a pdo command object
-    $cmd = $database->prepare($sql);
-    $cmd->bindParam(':firstName', $firstName, PDO::PARAM_STR, 50);
-    $cmd->bindParam(':lastName', $lastName, PDO::PARAM_STR, 50);
-    $cmd->bindParam(':time_pref', $time_pref, PDO::PARAM_STR,7);
-    $cmd->bindParam(':membership', $membership, PDO::PARAM_STR,40);
-    
-    if(!empty($memberId)) {
-        $cmd->bindParam(':memberId',$memberId,PDO::PARAM_INT);
-    }
+        $cmd = $database->prepare($sql);
+        $cmd->bindParam(':firstName', $firstName, PDO::PARAM_STR, 50);
+        $cmd->bindParam(':lastName', $lastName, PDO::PARAM_STR, 50);
+        $cmd->bindParam(':time_pref', $time_pref, PDO::PARAM_STR, 7);
+        $cmd->bindParam(':membership', $membership, PDO::PARAM_STR, 40);
+        $cmd->bindParam(':pic', $picName, PDO::PARAM_STR, 100);
+
+        if (!empty($memberId)) {
+            $cmd->bindParam(':memberId', $memberId, PDO::PARAM_INT);
+        }
+
 
 //executing
-    $cmd->execute();
+        $cmd->execute();
 
 //disconnect
-    $database = null;
-//show message to user
-    echo 'Details Saved';
+        $database = null;
+        //show message to user
+        echo 'Details Saved';
     }
-     catch (Exception $e) {
-        echo 'Please Refresh and Try Again';
+    catch (Exception $e) {
+        header('location:gym_error.php');
         exit();
     }
+
 }
     
 ?>
